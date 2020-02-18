bd13885a84d7b3909eea86b4f01894813a8429d7
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-02-18 21:59:42 +0100

    seed continue problem async

--- seed/seed.js
+++ seed/seed.js
@@ -1,88 +1,100 @@
 const axios = require("axios");
 const withDbConnection = require("../withDbConnection");
 const Recipes = require("../models/Recipe");
-const Users = require("../models/User");
+//const Users = require("../models/User");
 const Ingredients = require("../models/Ingredient");
 const { hashPassword, checkHashed } = require("../lib/hashing");
 
-const dataRecipes = []; //Array vacio para guardar objetos recetas
-const dataIngredients = []; //Array vacio para guardar objetos ingredientes
-const dataUsers = []; //Array vacio para guardar Users
+const recipeData = require("../data/recipe.json"); //trae data
+
+let dataUsers = []; //Array vacio para guardar Users
 
 const key = process.env.KEYAPI;
-const limit = 3;
+const limit = 1;
 
 //Pasamos por las limit recetas para obetener el objeto y la BASE URL
 for (let id = 1; id <= limit; id++) {
-  const baseURL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`;
-  getData(baseURL, id);
+  //const baseURL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`;
+  getData(recipeData);
 }
 
 //Obtenemos la baseURL para crear las colecciones
-function getData(baseURL, id) {
-  axios
-    .get(baseURL)
-    .then(dataRecipesload => {
-      //Función para ir recogiendo cada ingredients
-      createIngredients(dataRecipesload.data);
+//function getData(baseURL, id) {
+// axios
+// .get(baseURL)
+//.then(dataRecipesload => {
 
-      //Función para ir recogiendo cada receta e ingredients
-      createRecipes(dataRecipesload.data, dataIngredients);
+function getData(baseURL) {
+  //Función para ir recogiendo cada ingredients
+  createIngredientsAndRecipes(recipeData);
 
-      //Si id de las recetas coincide con el limit accedemos a base de datos y la creamos COLECCIONES
-      if (id == limit) {
-        //Función para crear USERS
-        createUsers();
-
-        withDbConnection(async () => {
-          await Ingredients.deleteMany();
-          await Recipes.deleteMany();
-          await Users.deleteMany();
-          await Ingredients.create(dataIngredients);
-          await Recipes.create(dataRecipes);
-          await Users.create(dataUsers);
-        });
-      }
-    })
-    .catch(err => console.log(err));
+  //Función para crear USERS
+  createUsers();
 }
 
 //creamos Ingredients
-function createIngredients(data) {
+function createIngredientsAndRecipes(data) {
   // Recoger datos requeridos Ingredients
-
   let ingredientsList = data.extendedIngredients.map(ingredient => {
     return ingredient.name;
   });
 
-  let objIngredient;
-
-  //Ingredient Object
-  ingredientsList.forEach(ingredients => {
-    objIngredient = {
+  //Ingredient array objects
+  const dataIngredients = ingredientsList.map(ingredients => {
+    return {
       name: ingredients
     };
-    dataIngredients.push(objIngredient);
   });
-}
 
-//creamos Recipes
-function createRecipes(data) {
   //console.log("dataIngredients", dataIngredients);
 
-  let ingredientsID = Ingredients.map(ingredient => ingredient.id);
+  withDbConnection(async () => {
+    await Ingredients.create(dataIngredients);
 
-  // Recoger datos requeridos Receta
-  const objRecipe = {
-    title: data.title,
-    time: data.readyInMinutes,
-    servings: data.servings,
-    types: data.dishTypes,
-    ingredients: ingredientsID,
-    description: data.instructions,
-    imageSrc: data.image
-  };
-  dataRecipes.push(objRecipe);
+    await getIdIngredients();
+
+    function getIdIngredients() {
+      let ingredientsID = dataIngredients.map(ingredient => {
+        return findIngredientsId(ingredient).then(ingredient => {
+          console.log(ingredient.id, "ingredient.id");
+          return ingredient.id;
+        });
+      });
+      console.log("ingredientsID", ingredientsID);
+
+      async function findIngredientsId(ingredient) {
+        let ingredientFound = await Ingredients.findOne({
+          name: ingredient.name
+        });
+        //console.log("ingredientFound", ingredientFound);
+        return ingredientFound;
+      }
+    }
+
+    // dataIngredients.forEach(ingredient => {
+    //   console.log(ingredient.name);
+    //   Ingredients.findOne({ name: ingredient.name })
+    //     .then(ingredientFound => {
+    //       //console.log(ingredientFound);
+    //       ingredientsID.push(ingredientFound._id);
+    //     })
+    //     .then(() => {
+    //       console.log("ingredientsID interior", ingredientsID);
+    //     });
+    // });
+
+    const dataRecipe = {
+      title: data.title,
+      time: data.readyInMinutes,
+      servings: data.servings,
+      types: data.dishTypes,
+      //ingredients: ingredientsID,
+      description: data.instructions,
+      imageSrc: data.image
+    };
+
+    await Recipes.create(dataRecipe);
+  });
 }
 
 //creamos USER data
@@ -93,6 +105,9 @@ function createUsers() {
       password: hashPassword("123456789"),
       name: "Rubén",
       lastname: "Vaquero",
+      street: "Calle Betron Ramírez nº10",
+      city: "Madrid",
+      country: "España",
       recipesFavourites: [],
       ingredientsList: [],
       rol: "admin"
@@ -102,6 +117,9 @@ function createUsers() {
       password: hashPassword("123456789"),
       name: "Pilar",
       lastname: "García",
+      street: "Bajada de la Libertad 23",
+      city: "Madrid",
+      country: "España",
       recipesFavourites: [],
       ingredientsList: [],
       rol: "admin"
@@ -111,9 +129,17 @@ function createUsers() {
       password: hashPassword("123456789"),
       name: "Juan",
       lastname: "Jiménez",
+      street: "Calle Luisa Lucas 51",
+      city: "Barcelona",
+      country: "España",
       recipesFavourites: [],
       ingredientsList: [],
       rol: "subscriptors"
     }
   ]);
+
+  withDbConnection(async () => {
+    await Users.deleteMany();
+    await Users.create(dataUsers);
+  });
 }
